7eac69d25dccc4d967511bbee72866c79ea4d4f3
Raul Arturo Ortiz Perez <rortizpe@everis.com>
2019-10-14 13:28:20 -0500

    add changes

--- src/main/java/com/example/example2/Example2Application.java
+++ src/main/java/com/example/example2/Example2Application.java
@@ -2,10 +2,9 @@ package com.example.example2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class Example2Application {
 
   public static void main(String[] args) {
